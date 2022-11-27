a728513cc94ead001427bf06048d3a2f7aec7d30
Ankur Banerjee <rukna2003@gmail.com>
2022-11-27 23:32:53 +0530

    -

---
+++ ShutXLibrary/Util.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+
+using System;
+
+namespace ShutXLibrary {
+    
+    public class Util {
+
+        private string Command;
+
+        public string GetCommand() {
+
+            return Command;
+
+        }
+
+        public void SetCommand(string Command) {
+        
+            this.Command = Command;
+            
+        }
+
+        public void ExecuteCommand() {
+
+            var processInfo = new ProcessStartInfo("cmd.exe", "/c " + Command);
+            
+            processInfo.CreateNoWindow = true;
+            
+            processInfo.UseShellExecute = false;
+            
+            processInfo.RedirectStandardError = true;
+            
+            processInfo.RedirectStandardOutput = true;
+
+            var process = Process.Start(processInfo);
+
+            process.OutputDataReceived += (object sender, DataReceivedEventArgs e) => Console.WriteLine("output>>" + e.Data);
+            
+            process.BeginOutputReadLine();
+
+            process.ErrorDataReceived += (object sender, DataReceivedEventArgs e) => Console.WriteLine("error>>" + e.Data);
+            
+            process.BeginErrorReadLine();
+
+            process.WaitForExit();
+            
+            Console.WriteLine("ExitCode: {0}", process.ExitCode);
+            
+            process.Close();
+        
+        }
+
+    }
+
+}
--- ShutXUI/ShutXUI.cs
+++ ShutXUI/ShutXUI.cs
@@ -6,24 +6,34 @@ namespace ShutXUI {
 
         readonly ShutXLibrary.Time time;
 
+        readonly ShutXLibrary.Util util;
+
         public ShutXUI() {
         
             InitializeComponent();
 
             time = new ShutXLibrary.Time();
+
+            util = new ShutXLibrary.Util();
         
         }
 
         private void HoursTextBox_KeyPress(object sender, KeyPressEventArgs e) {
-            
-            // 13 -> Enter/Return ASCII
 
             if (e.KeyChar == (char) 13) {
 
                 time.SetHours(int.Parse(hoursTextBox.Text));
 
                 time.CalculateSeconds();
-                
+
+                string Command = "shutdown -s -t " + time.GetSeconds().ToString();
+
+                util.SetCommand(Command);
+
+                util.ExecuteCommand();
+
+                Application.Exit();
+
             }
 
         }
