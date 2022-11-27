c86f415f2f3232af0e9b2332e7c02dbabb28f80f
Ankur Banerjee <rukna2003@gmail.com>
2022-11-28 04:30:32 +0530

    -

--- ShutXUI/ShutXUI.Designer.cs
+++ ShutXUI/ShutXUI.Designer.cs
@@ -32,6 +32,7 @@
             this.hourLabel = new System.Windows.Forms.TextBox();
             this.minuteLabel = new System.Windows.Forms.TextBox();
             this.secondLabel = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // hourIncrementButton
@@ -160,12 +161,26 @@
             this.secondLabel.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.secondLabel.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.secondLabel_KeyPress);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Lucida Console", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(43, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 41);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "LAUNCH";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // ShutXUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateGray;
-            this.ClientSize = new System.Drawing.Size(241, 191);
+            this.ClientSize = new System.Drawing.Size(241, 264);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.secondLabel);
             this.Controls.Add(this.minuteLabel);
             this.Controls.Add(this.hourLabel);
@@ -195,6 +210,7 @@
         private System.Windows.Forms.TextBox hourLabel;
         private System.Windows.Forms.TextBox minuteLabel;
         private System.Windows.Forms.TextBox secondLabel;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ShutXUI/ShutXUI.cs
+++ ShutXUI/ShutXUI.cs
@@ -214,6 +214,7 @@ namespace ShutXUI {
             }
 
         }
+    
     }
 
 }
